5f8d21f0a8fcf837f58da9a947c983d601920794
yokoyamatoyou <134029955+yokoyamatoyou@users.noreply.github.com>
2025-07-06 18:58:17 +0900

    Introduce MessageMemory base class

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 """Public package interface."""
 
-from .memory import ConversationMemory, BaseMemory
+from .memory import ConversationMemory, MessageMemory, BaseMemory
 from .vector_memory import VectorMemory
 from .agent import ReActAgent, ToTAgent
 from .tools import get_web_scraper, get_sqlite_tool, Tool, execute_tool
@@ -9,6 +9,7 @@ from .logging_utils import setup_logging
 __all__ = [
     "ConversationMemory",
     "VectorMemory",
+    "MessageMemory",
     "BaseMemory",
     "ReActAgent",
     "ToTAgent",
